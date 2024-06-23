64ce806adac07bf6b939ed1297893bec399484fd
tamarans7171 <tamarans7171@gmail.com>
2024-06-23 23:14:04 +0300

    clean code 1 in addDress

--- src/components/addDress.js
+++ src/components/addDress.js
@@ -1,23 +1,13 @@
-import Select, { components } from "react-select";
-import axios from "axios";
 import React, { useState, useEffect } from "react";
-import Fab from "@mui/material/Fab";
-import AddIcon from "@mui/icons-material/Add";
-import FileBase64 from "react-file-base64";
+import "./addDress.css";
+import Select, { components } from "react-select";
+import { Fab, ImageList, ImageListItem } from "@mui/material";
+import { Checkroom, Add } from "@mui/icons-material";
 import { CirclePicker } from "react-color";
 import { connect, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
-import { border, borderColor, style } from "@mui/system";
-import { Checkroom } from "@mui/icons-material";
-import "./addDress.css";
-import {
-  API_URL,
-  doApiGet,
-  doApiMethod,
-  TOKEN_NAME,
-} from "../services/apiService";
+import { API_URL, doApiGet } from "../services/apiService";
+
 function mapStateToProps(state) {
   return {
     user: state.User.user,
@@ -25,17 +15,15 @@ function mapStateToProps(state) {
 }
 export default connect(mapStateToProps)(function AddDress(props) {
   const { user } = props;
-  const [temp, settemp] = useState([]);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [selectedOptions, setSelectedOptions] = useState([]);
-  const [items, setItems] = useState([]);
   const [subAreas, setSubAreas] = useState();
   const [areas, setAreas] = useState([]);
   const [sizeError, setSizeError] = useState("");
   const [areaChoosen, setAreaChoosen] = useState();
   const [colors, setColors] = useState([]);
-  const [colorChoosen, setColorChoosen] = useState();
+  const [images, setImages] = useState([]);
   const [newDress, setNewDress] = useState({
     description: "",
     landlord: user._id,
@@ -45,13 +33,11 @@ export default connect(mapStateToProps)(function AddDress(props) {
     style: selectedOptions,
     color: "",
     subArea: "",
-    images: items,
+    images: [],
+    defaultImage: images.length > 0 ? images[0] : "",
   });
-  const [setDresses, setSetDresses] = useState([]);
-  const [setDressesImages, setSetDressesImages] = useState([]);
   const [styles, setStyles] = useState();
   const [colorsDictionary, setColorsDictionary] = useState({});
-  const [image, setImage] = useState([]);
 
   useEffect(() => {
     getStyles();
@@ -60,9 +46,7 @@ export default connect(mapStateToProps)(function AddDress(props) {
   }, []);
 
   useEffect(() => {
-    console.log(user.firstName == undefined);
     getSubAreas();
-    console.log("ho");
   }, [areaChoosen]);
 
   async function getStyles() {
@@ -77,7 +61,7 @@ export default connect(mapStateToProps)(function AddDress(props) {
 
   async function getSubAreas() {
     if (areaChoosen) {
-      const subAreasByAreaData = doApiGet(
+      const subAreasByAreaData = await doApiGet(
         `${API_URL}/subAreas/getSubAreasByArea/${areaChoosen}`
       );
       setSubAreas(subAreasByAreaData.data);
@@ -85,48 +69,39 @@ export default connect(mapStateToProps)(function AddDress(props) {
   }
 
   async function getColors() {
-    await axios.get("http://localhost:3003/colors/getColors").then((res) => {
-      let temp = {};
-      console.log(res.data);
-      res.data.forEach((t) => {
-        temp[t.name] = t._id;
-      });
-      console.log(temp);
-      setColorsDictionary(temp);
-      let mapColors = res.data.map((c) => c.name);
-      setColors(mapColors);
+    const resColors = await doApiGet(`${API_URL}/colors/getColors`);
+    let tempDictionaryColors = {};
+    resColors.data.forEach((t) => {
+      tempDictionaryColors[t.name] = t._id;
     });
+    setColorsDictionary(tempDictionaryColors);
+    let mapColors = resColors.data.map((c) => c.name);
+    setColors(mapColors);
   }
 
   function onFileChange(e) {
-    setImage({ imgCollection: e.target.files });
     if (e.target.files && e.target.files[0]) {
-      settemp(Object.values(e.target.files).map((i) => URL.createObjectURL(i)));
+      setImages({ imgCollection: e.target.files });
     }
-    console.log(e.target.files);
   }
 
   async function saveNewDress() {
-    let temp = newDress;
+    let temp = {...newDress};
     temp.style = selectedOptions;
-    // temp.images=items
     if (
-      newDress.color == "" ||
-      newDress.size == "" ||
-      newDress.price == "" ||
-      newDress.style == [] ||
-      newDress.description == "" ||
-      image.length == 0
+      newDress.color === "" ||
+      newDress.size === "" ||
+      newDress.price === "" ||
+      newDress.style.length === 0 ||
+      newDress.description === "" ||
+      images.length === 0
     )
       alert("עליך למלא את כל הפרטים!");
     else {
-      console.log(temp);
-
       var formData = new FormData();
-      for (const key of Object.keys(image.imgCollection)) {
-        formData.append("imgCollection", image.imgCollection[key]);
+      for (const key of Object.keys(images.imgCollection)) {
+        formData.append("imgCollection", images.imgCollection[key]);
       }
-      console.log(":::::  " + formData + "  /////////////////////////");
       if (
         newDress.size < 0 ||
         (newDress.size > 20 && newDress.size < 34) ||
@@ -136,49 +111,20 @@ export default connect(mapStateToProps)(function AddDress(props) {
           "עליך להכניס מידה תקינה: לילדות בין 0-20, ולנשים בין 34-54"
         );
       } else {
-        if (setDresses.length > 0) {
-          let tempSetDresses = [...setDresses, newDress];
-          let tempSetDressesImage = [...setDressesImages, formData];
-          dispatch({ type: "UPDATEDRESSES", payload: tempSetDresses });
-          dispatch({ type: "UPDATEARRIMAGES", payload: tempSetDressesImage });
-        } else {
-          dispatch({ type: "UPDATEDRESS", payload: temp });
-          dispatch({ type: "UPDATEIMAGES", payload: formData });
-        }
+        dispatch({ type: "UPDATEDRESS", payload: temp });
+        dispatch({ type: "UPDATEIMAGES", payload: formData });
 
-        if (user.firstName == undefined) {
+        if (user.firstName === undefined) {
           navigate("/login", { flag: -1 });
         } else {
           navigate("/payments", { state: { sum: 30, type: "newDress" } });
         }
-        //      else {
-        //       alert("יש 😊 הצלחת להעלות את השמלה לאתר, השמלה רק צריכה לעבור את אישור המנהל.")
-        //             await axios.post("http://localhost:3030/dress/addDress", temp).then((res)=>{
-        //   console.log(res,"jjjjjjjjjjjjj");
-        // })
-        //       navigate("/allProducts")
-        //     }
       }
     }
-
-    console.log(temp);
-  }
-
-  function changeDefaultImage(obj) {
-    let tempItems = items;
-    tempItems = tempItems.map((item) => {
-      if (item.id == obj.id) return { ...item, defaultSelected: true };
-      else return { ...item, defaultSelected: false };
-    });
-    console.log(tempItems);
-    setItems(tempItems);
   }
 
   function handleChangeComplete(color, event) {
-    setColorChoosen(color.hex);
-    console.log(colorsDictionary[color.hex]);
     setNewDress({ ...newDress, color: colorsDictionary[color.hex] });
-    // console.log(state);
   }
 
   const InputOption = ({
@@ -226,25 +172,12 @@ export default connect(mapStateToProps)(function AddDress(props) {
         getStyles={getStyles}
         innerProps={props}
       >
-        <input
-          type="checkbox"
-          checked={isSelected}
-          onChange={(e) => {
-            console.log(e.target);
-          }}
-        />
+        <input type="checkbox" checked={isSelected} />
         {children}
       </components.Option>
     );
   };
-  // console.log(selectedOptions);
-  // const allOptions = [
-  //   { value: "option 1", label: "option 1" },
-  //   { value: "option 2", label: "option 2" },
-  //   { value: "option 3", label: "option 3" },
-  //   { value: "option 4", label: "option 4" }
-  // ];
-
+  console.log(Object.keys(colorsDictionary));
   const customStyles = {
     control: (base) => ({
       ...base,
@@ -258,54 +191,6 @@ export default connect(mapStateToProps)(function AddDress(props) {
     }),
   };
 
-  function addDressToSet() {
-    let temp = newDress;
-    temp.style = selectedOptions;
-    // temp.images=items
-    if (
-      newDress.color == "" ||
-      newDress.size == "" ||
-      newDress.price == "" ||
-      newDress.style == [] ||
-      newDress.description == "" ||
-      image.length == 0
-    )
-      alert("עליך למלא את כל הפרטים!");
-    else {
-      console.log(temp);
-      var formData = new FormData();
-      for (const key of Object.keys(image.imgCollection)) {
-        formData.append("imgCollection", image.imgCollection[key]);
-      }
-
-      if (
-        newDress.size < 0 ||
-        (newDress.size > 20 && newDress.size < 34) ||
-        newDress.size > 54
-      ) {
-        setSizeError(
-          "עליך להכניס מידה תקינה: לילדות בין 0-20, ולנשים בין 34-54"
-        );
-      } else {
-        setColorChoosen("");
-        setSetDresses([...setDresses, temp]);
-        setNewDress({
-          description: "",
-          landlord: user._id,
-          price: "",
-          size: "",
-          uploadDate: new Date(),
-          style: selectedOptions,
-          subArea: "",
-          images: items,
-        });
-        setSetDressesImages([...setDressesImages, formData]);
-        setImage([]);
-        setSelectedOptions([]);
-        setAreaChoosen("");
-      }
-    }
-  }
   return (
     <div className="addDress addDressPage">
       <article className="l-design-widhtAdd">
@@ -325,7 +210,6 @@ export default connect(mapStateToProps)(function AddDress(props) {
               }}
               className="input__fieldAdd"
               type="text"
-              placeholder=" "
             />
             <span className="input__labelAdd">תאור</span>
           </label>
@@ -388,7 +272,13 @@ export default connect(mapStateToProps)(function AddDress(props) {
               <CirclePicker
                 className="ciclePickerStyle"
                 styles={{ border: "black 2px solid" }}
-                color={colorChoosen}
+                color={
+                  newDress.color
+                    ? Object.keys(colorsDictionary).filter(
+                        (key, value) => colorsDictionary[key] === newDress.color
+                      )[0]
+                    : ""
+                }
                 onChangeComplete={handleChangeComplete}
                 colors={colors}
               />
@@ -451,7 +341,6 @@ export default connect(mapStateToProps)(function AddDress(props) {
               name="imgCollection"
               onChange={(e) => {
                 onFileChange(e);
-                console.log(e.target.files);
               }}
               multiple
             />
@@ -466,38 +355,33 @@ export default connect(mapStateToProps)(function AddDress(props) {
               className="addDressBtn"
               aria-label="add"
             >
-              <AddIcon />
-              {newDress.landlord == undefined
+              <Add />
+              {newDress.landlord === undefined
                 ? "הרשם/התחבר ועדכן שמלה"
                 : "עדכן שמלה"}
             </Fab>
-            {/* <Fab variant="extended" onClick={addDressToSet} size="medium" color="secondary" aria-label="add">
-             <AddIcon />
-             {"הוסף שמלה לסט"}
-            </Fab> */}
           </div>
         </div>
       </article>
-      {/* {setDresses.length > 0 && <h3>כמות השמלות בסט: {setDresses.length}</h3>}
-      {temp && temp.length > 0 && (
+
+      {images.imgCollection && (
         <div>
           <h3>בחר את התמונה שתוגדר כברירת מחדל בתצוגה</h3>
           <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-            {temp.map((item, i) => (
+            {Object.values(images.imgCollection).map((item, i) => (
               <ImageListItem
                 onClick={() => {
-                  changeDefaultImage(item);
+                  setNewDress({ ...newDress, defaultImage: item });
                 }}
                 key={i}
               >
                 <img
                   style={
-                    item.defaultSelected
+                    item === newDress.defaultImage
                       ? { border: "3px solid red" }
                       : { border: "3px solid black" }
                   }
-                  src={`${item.name}`}
-                  srcSet={`${item}`}
+                  src={`${URL.createObjectURL(item)}`}
                   alt={item.name}
                   loading="lazy"
                 />
@@ -505,38 +389,7 @@ export default connect(mapStateToProps)(function AddDress(props) {
             ))}
           </ImageList>
         </div>
-      )} */}
-
-      <svg xmlns="http://www.w3.org/2000/svg" className="hiddenAdd">
-        <symbol id="icon-coffee" viewBox="0 0 20 20">
-          <title>icon-coffee</title>
-          <path
-            fill="currentColor"
-            d="M15,17H14V9h3a3,3,0,0,1,3,3h0A5,5,0,0,1,15,17Zm1-6v3.83A3,3,0,0,0,18,12a1,1,0,0,0-1-1Z"
-          />
-          <rect
-            fill="currentColor"
-            x="1"
-            y="7"
-            width="15"
-            height="12"
-            rx="3"
-            ry="3"
-          />
-          <path
-            fill="var(--color-accent)"
-            d="M7.07,5.42a5.45,5.45,0,0,1,0-4.85,1,1,0,0,1,1.79.89,3.44,3.44,0,0,0,0,3.06,1,1,0,0,1-1.79.89Z"
-          />
-          <path
-            fill="var(--color-accent)"
-            d="M3.07,5.42a5.45,5.45,0,0,1,0-4.85,1,1,0,0,1,1.79.89,3.44,3.44,0,0,0,0,3.06,1,1,0,1,1-1.79.89Z"
-          />
-          <path
-            fill="var(--color-accent)"
-            d="M11.07,5.42a5.45,5.45,0,0,1,0-4.85,1,1,0,0,1,1.79.89,3.44,3.44,0,0,0,0,3.06,1,1,0,1,1-1.79.89Z"
-          />
-        </symbol>
-      </svg>
+      )}
     </div>
   );
 });
