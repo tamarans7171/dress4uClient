17359c264bfc434cac50716cc4b7d52534451046
tamarans7171 <tamarans7171@gmail.com>
2024-06-23 21:48:02 +0300

    mm

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,37 +1,35 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:3003"
-export const TOKEN_NAME = "MYSECRETWORD"
+export const API_URL = "http://localhost:3003";
+export const TOKEN_NAME = "MYSECRETWORD";
 
-export const doApiGet = async(_url) => {
-  try{
-    let resp = await axios.get(_url,{
-      headers:{
-        "x-api-key": localStorage[TOKEN_NAME]
-      }
-    })
+export const doApiGet = async (_url) => {
+  try {
+    let resp = await axios.get(_url, {
+      headers: {
+        "x-api-key": localStorage[TOKEN_NAME],
+      },
+    });
     return resp;
-  }
-  catch(err){
+  } catch (err) {
     throw err;
   }
-}
+};
 
 // For Post,delete, put, patch
-export const doApiMethod = async(_url,_method,_body = {}) => {
-  try{
-    console.log(_body)
+export const doApiMethod = async (_url, _method, _body = {}) => {
+  try {
+    console.log(_body);
     let resp = await axios({
-      url:_url,
-      method:_method,
-      data:_body,
-      headers:{
-        "x-api-key":localStorage[TOKEN_NAME]
-      }
-    })
+      url: _url,
+      method: _method,
+      data: _body,
+      headers: {
+        "x-api-key": localStorage[TOKEN_NAME],
+      },
+    });
     return resp;
-  }
-  catch(err){
+  } catch (err) {
     throw err;
   }
-}
+};
